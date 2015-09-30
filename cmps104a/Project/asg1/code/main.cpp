7c18af4d9ef175d1ceb4a36b8a8d06fecb3bde2e
adam <akpinarb@ucsc.edu>
2015-09-29 22:40:06 -0700

    started working on asg1

---
+++ cmps104a/Project/asg1/code/main.cpp
@@ -0,0 +1,11 @@
+//Adam Pinarbasi
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main (int argc, char** argv) {
+}
--- cmps104a/Project/asg1/examples/utility-code/auxlib.cpp
+++ cmps104a/Project/asg1/examples/utility-code/auxlib.cpp
@@ -52,6 +52,10 @@ void eprint_status (const char* command, int status) {
    eprintf ("\n");
 }
 
+int get_exitstatus (void) {
+   return exitstatus;
+}
+
 void veprintf (const char* format, va_list args) {
    assert (execname != NULL);
    assert (format != NULL);
@@ -83,10 +87,6 @@ void syserrprintf (const char* object) {
    errprintf ("%:%s: %s\n", object, strerror (errno));
 }
 
-int get_exitstatus (void) {
-   return exitstatus;
-}
-
 void set_exitstatus (int newexitstatus) {
    if (exitstatus < newexitstatus) exitstatus = newexitstatus;
    DEBUGF ('x', "exitstatus = %d\n", exitstatus);
@@ -103,6 +103,7 @@ void __stubprintf (const char* file, int line, const char* func,
    fflush (NULL);
 }     
 
+
 void set_debugflags (const char* flags) {
    debugflags = flags;
    if (strchr (debugflags, '@') != NULL) alldebugflags = true;
@@ -127,5 +128,5 @@ void __debugprintf (char flag, const char* file, int line,
    fflush (NULL);
 }
 
-RCSC("$Id: auxlib.cpp,v 1.2 2014-10-07 18:07:29-07 - - $")
+RCSC("$Id: auxlib.cpp,v 1.1 2014-10-03 18:22:05-07 - - $")
 
