4eca8876adc1e4dd89f3e8141d0f95d93129d76c
adam <akpinarb@ucsc.edu>
2015-10-05 14:54:33 -0700

    working on asg1 for cmps112

---
+++ cmps104a/Project/asg1/code/oc.cpp
@@ -0,0 +1,12 @@
+//Adam Pinarbasi
+//oc.cpp
+//asg1
+
+#include <iomanip>
+#include <unordered_set>
+using namespace std;
+
+int main (int argc, char** argv) {
+   cout << "hello, world" << endl;
+   return EXIT_SUCCESS;
+}
---
+++ cmps112/Assignments/asg1-scheme-sbi/hello.scm
@@ -0,0 +1,6 @@
+#!/afs/cats.ucsc.edu/courses/cmps112-wm/usr/racket/bin/mzscheme -qr
+
+(begin
+  (display "Hello, world!")
+  (newline))
+
