de673765fb419eee77ed2370f8846a590b067c4f
adam <akpinarb@ucsc.edu>
2015-10-08 11:29:19 -0700

    almost finished with cmps104a asg1

--- cmps104a/Assignments/asg1/code/main.cpp
+++ cmps104a/Assignments/asg1/code/main.cpp
@@ -10,6 +10,8 @@
 
 using namespace std;
 
+#include <stdio.h>
+#include <errno.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -27,7 +29,7 @@ static void chomp (char* string, char delim) {
    if (*nlpos == delim) *nlpos = '\0';
 }
 
-void cpplines (FILE* pipe, char* filename) {
+void cpplines (FILE* pipe, char* filename, ostream& out) {
    int linenr = 1;
    char inputname[LINESIZE];
    strcpy (inputname, filename);
@@ -36,12 +38,12 @@ void cpplines (FILE* pipe, char* filename) {
       char* fgets_rc = fgets (buffer, LINESIZE, pipe);
       if (fgets_rc == NULL) break;
       chomp (buffer, '\n');
-      printf ("%s:line %d: [%s]\n", filename, linenr, buffer);
+      //printf ("%s:line %d: [%s]\n", filename, linenr, buffer);
       // http://gcc.gnu.org/onlinedocs/cpp/Preprocessor-Output.html
       int sscanf_rc = sscanf (buffer, "# %d \"%[^\"]\"",
                               &linenr, filename);
       if (sscanf_rc == 2) {
-         printf ("DIRECTIVE: line %d file \"%s\"\n", linenr, filename);
+         //printf ("DIRECTIVE: line %d file \"%s\"\n", linenr, filename);
          continue;
       }
       char* savepos = NULL;
@@ -50,8 +52,10 @@ void cpplines (FILE* pipe, char* filename) {
          char* token = strtok_r (bufptr, " \t\n", &savepos);
          bufptr = NULL;
          if (token == NULL) break;
-         printf ("token %d.%d: [%s]\n",
-                 linenr, tokenct, token);
+         intern_stringset(token);
+         dump_stringset(out);
+         /*printf ("token %d.%d: [%s]\n",
+                 linenr, tokenct, token);*/
       }
       ++linenr;
    }
@@ -84,29 +88,49 @@ static void perform_op (int argc, char **argv) {
             abort();
       }
    }
-}
-      
 
-int main (int argc, char** argv) {
-   perform_op(argc, argv);
    if (optind != (argc - 1)) {
-      for (int i = optind; i < (argc - 1); ++i) 
+      for (int i = optind; i < (argc - 1); ++i)
          cerr << "Non-option argument: " << argv[i] << endl;
-      return EXIT_FAILURE;
+      exit(EXIT_FAILURE);
    }
+}
 
-   string filename(argv[argc - 1]);
+static string check_suffix (int argc, char** argv) {
+   string filename(basename(argv[argc - 1]));
    string suffix = filename.substr(filename.length() - 3, 3);
    string compare_str(".oc");
    if (suffix.compare(compare_str) != 0) {
       cerr << "Usage: oc [-ly] [-@ flag...] [-D string] program.oc" << endl;
-      return EXIT_FAILURE;
+      exit(EXIT_FAILURE);
    }
+   string outfile_name = filename.substr(0, filename.length() - 3) + ".str"; 
+   return outfile_name;
+}
 
-   const char* insert = "hello, world";
-   intern_stringset(insert);
-   ofstream outfile("program.str");
-   dump_stringset(outfile);
+static void insert_set (ifstream& infile, ofstream& outfile, 
+                                          char* infile_name) {
+   string command = CPP + " " + infile_name;
+   FILE* pipe = popen(command.c_str(), "r");
+   if (pipe == NULL) {
+      cerr << "Could not perform: " << command << endl;
+      exit(EXIT_FAILURE);
+   }
+   else {
+      cpplines(pipe, infile_name, outfile);
+      int pclose_rc = pclose(pipe);
+      //eprint_status(command.c_str(), pclose_rc);
+   }
+}
+
+int main (int argc, char** argv) {
+   perform_op(argc, argv);
+   string outfile_name = check_suffix(argc, argv);
+
+   ifstream infile(argv[argc - 1]);
+   ofstream outfile(outfile_name);
+   insert_set(infile, outfile, argv[argc - 1]);
+   infile.close();
    outfile.close();
    return EXIT_SUCCESS;
 }
