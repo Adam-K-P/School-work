a0e20755f7866a54813ee7a4caa34e2bdf0e15d8
Adam-K-P <akpinarb@ucsc.edu>
2015-12-07 22:43:21 -0800

    studying for finals in 112 and 104a

--- cmps104a/Assignments/asg5/code/emitter.cpp
+++ cmps104a/Assignments/asg5/code/emitter.cpp
@@ -1,4 +1,5 @@
-// $Id: emitter.cpp,v 1.2 2015-07-09 18:25:23-07 - - $
+//Adam Pinarbasi
+//akpinarb
 
 #include <assert.h>
 #include <stdio.h>
@@ -17,15 +18,304 @@
 using namespace std;
 
 /* register numbers */
-int a_num = 1;
+int i_num = 1;
 int c_num = 1;
 int p_num = 1;
 int s_num = 1;
 int b_num = 1;
 
+/* register stacks */
+vector<int> i_reg_stack;
+vector<int> c_reg_stack;
+vector<int> p_reg_stack;
+vector<int> s_reg_stack;
+vector<int> b_reg_stack;
+
 deque<string> string_cons;
 
-void emit_assign (astree* tree) {}
+/* Necessary forward declarations */
+void emit_expr (astree*);
+void emit_exprs (astree*);
+void emit_assign (astree*);
+void emit_stmts (astree*);
+
+void emit_assign (astree* tree) {
+   astree* left = tree->children.at(0);
+   astree* right = tree->children.at(1);
+   if (right->attributes[ATTR_const]) {
+      if (not tree->blocknr) {
+         fprintf (oilfile, "        __%s = %s;\n",
+                     left->children.at(0)->lexinfo->c_str(),
+                     right->lexinfo->c_str());
+      }
+      else {
+         fprintf (oilfile, "        _%lu_%s = %s;\n",
+                     tree->blocknr,
+                     left->children.at(0)->lexinfo->c_str(),
+                     right->lexinfo->c_str());
+      }
+   }
+   else {
+      emit_exprs (right);
+      if (not tree->blocknr) {
+         if (left->attributes[ATTR_int]) {
+            fprintf (oilfile, "        __%s = i%d;\n",
+                     left->lexinfo->c_str(),
+                     i_num++);
+         }
+         if (left->attributes[ATTR_bool] or
+             left->attributes[ATTR_char]) {
+            fprintf (oilfile, "        __%s = c%d;\n",
+                     left->lexinfo->c_str(),
+                     c_num++);
+         }
+         if (left->attributes[ATTR_string]) {
+            fprintf (oilfile, "        __%s = p%d;\n",
+                     left->lexinfo->c_str(),
+                     p_num++);
+         }
+      }
+      else {
+         if (left->attributes[ATTR_int]) {
+            fprintf (oilfile, "        _%lu_%s = i%d;\n",
+                     left->blocknr,
+                     left->lexinfo->c_str(),
+                     i_num++);
+         }
+         if (left->attributes[ATTR_bool] or
+             left->attributes[ATTR_char]) {
+            fprintf (oilfile, "        _%lu_%s = c%d;\n",
+                     left->blocknr,
+                     left->lexinfo->c_str(),
+                     c_num++);
+         }
+         if (left->attributes[ATTR_string]) {
+            fprintf (oilfile, "        _%lu_%s = p%d;\n",
+                     left->blocknr,
+                     left->lexinfo->c_str(),
+                     p_num++);
+         }
+      }
+   }
+}
+
+void emit_vardec (astree* tree) {
+   astree* left = tree->children.at(0);
+   astree* right = tree->children.at(1);
+   if (right->attributes[ATTR_const]) {
+      if (not tree->blocknr) {
+         fprintf (oilfile, "        __%s = %s;\n", 
+                        left->children.at(0)->lexinfo->c_str(),
+                        right->lexinfo->c_str());
+      }
+      else {
+         fprintf (oilfile, "        %s _%lu_%s = %s;\n",
+                        left->lexinfo->c_str(),
+                        tree->blocknr,
+                        left->children.at(0)->lexinfo->c_str(),
+                        right->lexinfo->c_str());
+      }
+   }
+   else {
+      emit_exprs (right);
+      if (not tree->blocknr) {
+         if (left->attributes[ATTR_int]) {
+            fprintf (oilfile, "        __%s = i%d;\n",
+                        left->children.at(0)->lexinfo->c_str(),
+                        i_num++);
+         }
+         if (left->attributes[ATTR_bool] or
+             left->attributes[ATTR_string] or
+             left->attributes[ATTR_char]) {
+            fprintf (oilfile, "        __%s = c%d;\n",
+                        left->children.at(0)->lexinfo->c_str(),
+                        c_num++);
+         }
+      }
+      else {
+         if (left->attributes[ATTR_int]) {
+            fprintf (oilfile, "        %s _%lu_%s = i%d;\n",
+                        left->lexinfo->c_str(),
+                        left->blocknr,
+                        left->children.at(0)->lexinfo->c_str(),
+                        i_num++);
+         }
+         if (left->attributes[ATTR_bool]) {
+            fprintf (oilfile, "        char _%lu_%s = b%d;\n",
+                        left->blocknr,
+                        left->children.at(0)->lexinfo->c_str(),
+                        b_num++);
+         }
+         if (left->attributes[ATTR_char]) {
+            fprintf (oilfile, "        char _%lu_%s = c%d;\n",
+                        left->blocknr,
+                        left->children.at(0)->lexinfo->c_str(),
+                        c_num++);
+         }
+         if (left->attributes[ATTR_string]) {
+            fprintf (oilfile, "        char* _%lu_%s = p%d;\n",
+                        left->blocknr,
+                        left->children.at(0)->lexinfo->c_str(),
+                        p_num++);
+         }
+      }
+   }
+}
+
+void emit_bin (astree* tree, char const* op, char reg, int r_num) {
+   astree* left = tree->children.at(0);
+   astree* right = tree->children.at(1);
+   if ((left->symbol == TOK_IDENT or
+        left->symbol == NUMBER) and 
+       (right->symbol == TOK_IDENT or
+        right->symbol == NUMBER)) {
+      fprintf (oilfile, "        %c%d = %s %s %s;\n",
+                  reg,
+                  r_num,
+                  left->lexinfo->c_str(),
+                  op,
+                  right->lexinfo->c_str());
+   }
+}
+
+void emit_expr (astree* tree) {
+   switch (tree->symbol) {
+   
+      case TOK_VARDECL: 
+         emit_vardec (tree);
+         break;
+
+      case '=': 
+         emit_assign (tree);
+         break;
+
+      case '+':
+         emit_bin (tree, "+", 'i', i_num);
+         break;
+
+      case '-':
+         emit_bin (tree, "-", 'i', i_num);
+         break;
+
+      case '*':
+         emit_bin (tree, "*", 'i', i_num);
+         break;
+
+      case '/':
+         emit_bin (tree, "/", 'i', i_num);
+         break;
+
+      case '<':
+         emit_bin (tree, "<", 'b', b_num);
+         break;
+
+      case '>':
+         emit_bin (tree, ">", 'b', b_num);
+         break;
+
+      case BOOL_EQ:
+         emit_bin (tree, "==", 'b', b_num);
+         break;
+
+      case BOOL_LESS_EQ:
+         emit_bin (tree, "<=", 'b', b_num);
+         break;
+
+      case BOOL_GRT_EQ:
+         emit_bin (tree, ">=", 'b', b_num);
+         break;
+
+      case BOOL_NOT_EQ:
+         emit_bin (tree, "!=", 'b', b_num);
+         break;
+
+      case TOK_CALL:
+         break;
+
+      case TOK_KW_RETURN:
+         fprintf (oilfile, "        return %s;\n", 
+                           tree->children.at(0)->lexinfo->c_str());
+         break;
+
+      case TOK_RETURNVOID:
+         fprintf (oilfile, "        return;\n");
+         break;
+
+      default: break;
+   }
+}
+
+void emit_exprs (astree* tree) {
+   for (size_t i = 0; i < tree->children.size(); ++i) {
+      if (tree->children.at(i)->symbol == TOK_FUNCTION) continue;
+      emit_exprs (tree->children.at(i));
+   }
+   emit_expr (tree);
+}
+
+/* Various statement emission functions */
+
+void emit_while (astree* tree) {
+   fprintf (oilfile, "while_%lu_%lu_%lu:;\n", tree->lloc.filenr,
+                                              tree->lloc.linenr,
+                                              tree->lloc.offset);
+   if (tree->children.at(0)->symbol == TOK_IDENT) 
+      fprintf (oilfile, "if (!%s) goto break_%lu_%lu_%lu:;\n",
+               tree->children.at(0)->lexinfo->c_str(),
+               tree->lloc.filenr,
+               tree->lloc.linenr,
+               tree->lloc.offset);
+   else {
+      emit_exprs (tree->children.at(0));
+      fprintf (oilfile, "if (!b%d) goto break_%lu_%lu_%lu;\n",
+               c_num,
+               tree->lloc.filenr,
+               tree->lloc.linenr,
+               tree->lloc.offset);
+   }
+   emit_stmts (tree->children.at(1));
+   fprintf (oilfile, "goto while_%lu_%lu_%lu;\n"
+                     "break_%lu_%lu_%lu:;\n",
+               tree->lloc.filenr,
+               tree->lloc.linenr,
+               tree->lloc.offset,
+               tree->lloc.filenr,
+               tree->lloc.linenr,
+               tree->lloc.offset);
+}
+
+void emit_if (astree* tree) {
+   fprintf (oilfile, "if_%lu_%lu_%lu:;\n", tree->lloc.filenr,
+                                           tree->lloc.linenr,
+                                           tree->lloc.offset);
+   if (tree->children.at(0)->symbol == TOK_IDENT) {
+      if (tree->children.size() == 3) {
+         fprintf (oilfile, "if (!b%d) goto else_%lu_%lu_%lu;\n",
+                  b_num,
+                  tree->lloc.filenr,
+                  tree->lloc.linenr,
+                  tree->lloc.offset);
+         emit_exprs (tree->children.at(1));
+         fprintf (oilfile, "else_%lu_%lu_%lu;\n",
+                  tree->lloc.filenr,
+                  tree->lloc.linenr,
+                  tree->lloc.offset);
+      }
+      else {
+         fprintf (oilfile, "if (!b%d) goto fi_%lu_%lu_%lu;\n",
+                  b_num,
+                  tree->lloc.filenr,
+                  tree->lloc.linenr,
+                  tree->lloc.offset);
+         emit_stmts (tree->children.at(2)->children.at(0));
+         fprintf (oilfile, "fi_%lu_%lu_%lu;\n",
+                  tree->lloc.filenr,
+                  tree->lloc.linenr,
+                  tree->lloc.offset);
+      }
+   }
+   else emit_exprs (tree->children.at(0));
+}
 
 void emit_stmts (astree* tree) {
    switch (tree->symbol) {
@@ -36,54 +326,15 @@ void emit_stmts (astree* tree) {
          break;
 
       case TOK_KW_WHILE:
-         fprintf (oilfile, "while_%lu_%lu_%lu:;\n", tree->lloc.filenr,
-                                                    tree->lloc.linenr,
-                                                    tree->lloc.offset);
+         emit_while (tree);
          break;
 
       case TOK_KW_IF:
-         fprintf (oilfile, "if_%lu_%lu_%lu:;\n", tree->lloc.filenr,
-                                                 tree->lloc.linenr,
-                                                 tree->lloc.offset);
+         emit_if (tree);
          break;
 
-      case TOK_VARDECL:
-      case '=':
-         emit_assign (tree);
-         break;
+      default: emit_exprs (tree);
 
-      /* each of these needs to handle the left side and right side */
-
-      case '!':
-         break;
-
-      case '+':
-         break;
-
-      case '-':
-         break;
-
-      case '*':
-         break;
-
-      case '/':
-         break;
-
-      case '<':
-         break;
-
-      case '>':
-         break;
-
-      case TOK_CALL:
-         break;
-
-      case TOK_KW_RETURN:
-         break;
-
-      case TOK_RETURNVOID:
-         break;
-      
    }
 }
 
@@ -185,6 +436,7 @@ void emit_func (astree* tree) {
    emit_params (tree->children.at(1)->children.at(0));
    fprintf (oilfile, "{\n");
    emit_stmts (tree->children.at(2));
+   fprintf (oilfile, "}\n");
 }
 
 void emit_fcns (astree* tree) {
@@ -193,6 +445,7 @@ void emit_fcns (astree* tree) {
          emit_func (tree->children.at(i));
    fprintf (oilfile, "void __ocmain (void)\n{\n");
    emit_stmts (tree);
+   fprintf (oilfile, "}\n");
 }
 
 /* Program emission */
---
+++ cmps112/Midterm/practice.perl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+print while <>;
+
+sub range {
+   my $x = shift;
+   my $y = shift;
+   return $x..$y;
+}
+   
